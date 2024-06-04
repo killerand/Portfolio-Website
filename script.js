2583ff9e5bba65153dae6739e3de125ac121b822
Asiya Bint <163452363+killerand@users.noreply.github.com>
2024-06-04 13:39:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('black-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 2000); // 2000 milliseconds = 2 seconds
+});
+
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
